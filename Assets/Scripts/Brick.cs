368109eedb0ded9f8136921af563bb7823e3c196
EricLin252 <jamielin61207@gmail.com>
2020-05-31 07:13:06 +0800

    prefab

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour{
+	public float speed;
+	// Start is called before the first frame update
+	void Start(){
+	}
+
+	// Update is called once per frame
+	void Update(){
+		transform.Translate(0, 0, -speed*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			Destroy(gameObject);
+		}
+	}
+}
