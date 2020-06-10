674b2fc6018dca9ba87fbdbe6c2eac1fd619dc2c
EricLin252 <jamielin61207@gmail.com>
2020-06-11 06:18:58 +0800

    add score

---
+++ Assets/Scripts/GetScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetScore : MonoBehaviour{
+	GameObject Player;
+	// Start is called before the first frame update
+	void Start(){
+		Player = GameObject.FindWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update(){
+		if(Player) gameObject.GetComponent<Text>().text = "" + Player.GetComponent<Player>().score;
+	}
+}
